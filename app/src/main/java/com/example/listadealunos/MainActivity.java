675ec3de5888d192b9f8524638a414ce6861cae4
lericardolima <lericardolima@gmail.com>
2020-08-19 21:50:30 -0300

    Exibindo a lista dinamicamente

--- app/src/main/java/com/example/listadealunos/MainActivity.java
+++ app/src/main/java/com/example/listadealunos/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.listadealunos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +19,11 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Lionel Messi", "Cristiano Ronaldo", "Neymar Junior"));
+
+        ListView listViewAlunos = findViewById(R.id.activity_main_list_alunos);
+        listViewAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
+
     }
 }
