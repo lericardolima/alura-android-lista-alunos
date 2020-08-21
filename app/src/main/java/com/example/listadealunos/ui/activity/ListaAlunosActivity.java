b314556da24e4926a121f1dc5b49276ada48a0b6
lericardolima <lericardolima@gmail.com>
2020-08-21 20:37:40 -0300

    Refatorar estrutura de código

--- app/src/main/java/com/example/listadealunos/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/listadealunos/ui/activity/ListaAlunosActivity.java
@@ -1,6 +1,5 @@
-package com.example.listadealunos;
+package com.example.listadealunos.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -8,21 +7,24 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.listadealunos.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_alunos);
 
         List<String> alunos = new ArrayList<>(Arrays.asList("Lionel Messi", "Cristiano Ronaldo", "Neymar Junior"));
 
-        ListView listViewAlunos = findViewById(R.id.activity_main_list_alunos);
+        ListView listViewAlunos = findViewById(R.id.activity_list_alunos);
         listViewAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
 
     }
+
 }
