44042d7f2b013658c25a277cd0ab7b8daceeca0b
lericardolima <lericardolima@gmail.com>
2020-08-26 20:15:03 -0300

    Mudar de OnClickListener para OmItemClickListener

--- app/src/main/java/com/example/listadealunos/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/listadealunos/ui/activity/ListaAlunosActivity.java
@@ -2,7 +2,9 @@ package com.example.listadealunos.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -34,5 +36,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onResume();
         ListView listViewAlunos = findViewById(R.id.activity_list_alunos);
         listViewAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, SingletonAlunoDAO.getInstance().listar()));
+        listViewAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("POSICAO_ALUNO", "onItemClick: " + position);
+            }
+        });
     }
 }
